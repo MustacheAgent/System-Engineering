a7c50c47c3dfe7484df31c0c5ef0c0139500e0d8
Dmitry <dimonus98@yandex.ru>
2021-09-21 19:38:32 +0300

    TCP клиент и сервер

---
+++ TCP&UDP/TCP Server/TCP Server/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCP_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+
+            Console.WriteLine("IP адрес: ");
+            string ip = Console.ReadLine();
+
+            Console.WriteLine("Порт: ");
+            int port = int.Parse(Console.ReadLine());
+
+            try
+            {
+                IPAddress localAddr = IPAddress.Parse(ip);
+                server = new TcpListener(localAddr, port);
+
+                // запуск слушателя
+                server.Start();
+
+                while (true)
+                {
+                    Console.WriteLine("Ожидание подключений... ");
+
+                    // получаем входящее подключение
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Подключен клиент. Выполнение запроса...");
+
+                    // получаем сетевой поток для чтения и записи
+                    NetworkStream stream = client.GetStream();
+
+                    // сообщение для отправки клиенту
+                    string response = "Привет мир";
+                    // преобразуем сообщение в массив байтов
+                    byte[] data = Encoding.UTF8.GetBytes(response);
+
+                    // отправка сообщения
+                    stream.Write(data, 0, data.Length);
+                    Console.WriteLine("Отправлено сообщение: {0}", response);
+                    // закрываем поток
+                    stream.Close();
+                    // закрываем подключение
+                    client.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (server != null)
+                    server.Stop();
+            }
+        }
+    }
+}
