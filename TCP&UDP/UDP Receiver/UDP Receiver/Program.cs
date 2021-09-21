e0843d059eb24b3928a051f0a1c8be5563b4231f
Dmitry <dimonus98@yandex.ru>
2021-09-21 20:18:12 +0300

    UDP протокол

---
+++ TCP&UDP/UDP Receiver/UDP Receiver/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP_Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Порт: ");
+            int port = int.Parse(Console.ReadLine());
+            UdpClient receiver = new(port);
+
+            try
+            {
+                IPEndPoint ipAddr = null;
+                byte[] data = receiver.Receive(ref ipAddr);
+                string message = Encoding.UTF8.GetString(data);
+                Console.WriteLine("Принято сообщение: {0}", message);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+            }
+
+            receiver.Close();
+            Console.WriteLine("Обмен завершен");
+        }
+    }
+}
---
+++ TCP&UDP/UDP Transmitter/UDP Transmitter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP_Transmitter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("IP адрес: ");
+            string ip = Console.ReadLine();
+
+            Console.WriteLine("Порт: ");
+            int port = int.Parse(Console.ReadLine());
+            UdpClient transmitter = new(ip, port); ;
+
+            try
+            {
+                //transmitter = new(ip, port);
+                string message = "Привет мир!";
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                int bytesSent = transmitter.Send(data, data.Length);
+            }
+            catch(SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+            }
+
+            transmitter.Close();
+            Console.WriteLine("Обмен завершен");
+        }
+    }
+}
